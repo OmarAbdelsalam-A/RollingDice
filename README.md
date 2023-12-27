58f8d90b7a4a9b04afc02ad3a44c0ce60b9dbaad
OmarAbdelsalam-A <omar.abdelsalam.a@gmail.com>
2023-12-27 19:05:54 +0200

    Finished Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rolling Dice
+
+A flutter app that can be used instead of rolling dices
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple[200],
+        appBar: AppBar(
+          title: Text('Dice'),
+          backgroundColor: Colors.deepPurple[300],
+          centerTitle: true,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void randomize() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () {
+                randomize();
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: () {
+                randomize();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
